54cf2d6d845cbf8ef5e64bdbb0f5ffad3c833d30
Saeed Al Berzawi <saidberzawi@gmail.com>
2022-04-29 16:57:26 +0300

    UNION

---
+++ main.js
@@ -0,0 +1,33 @@
+// header scroll
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// swiper
+var swiper = new Swiper(".home", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+
+});
